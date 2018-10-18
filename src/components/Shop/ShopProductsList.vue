c6a7b3a68abeb07ad3e17b2ed172b197b93e9eeb
hades <quentindevial19@gmail.com>
2018-10-18 13:48:20 +0200

    first commit

---
+++ src/components/Shop/ShopProductsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex flex-row flex-wrap p-1">
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+    <shop-product-item></shop-product-item>
+  </div>
+</template>
+
+<script>
+  import ShopProductItem from './ShopProductItem.vue';
+
+export default {
+  components: {
+    ShopProductItem
+  }
+}
+
+</script>
+
+<style>
+
+</style>
